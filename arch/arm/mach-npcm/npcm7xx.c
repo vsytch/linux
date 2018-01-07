c4b5869af7bdf9f578cd6fab51dd5473424cdf9e
Tomer Maimon <tmaimon77@gmail.com>
2018-01-07 11:42:55 +0200

    arm: npcm: add basic support for Nuvoton BMCs
    Adds basic support for the Nuvoton NPCM750 BMC.
    
    Based on [v8,1/3] arm: npcm: add basic support for Nuvoton BMCs patch:
    Brendan Higgins : - https://patchwork.kernel.org/patch/10063409/
    
    Signed-off-by: Tomer Maimon <tmaimon77@gmail.com>
    
    Modify NPCM7xx configuration to support NPCM730 configuration
    
    Signed-off-by: Tomer Maimon <tmaimon77@gmail.com>

---
+++ arch/arm/mach-npcm/npcm7xx.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2014-2017 Nuvoton Technology corporation.
+ * Copyright 2017 Google, Inc.
+ *
+ * Released under the GPLv2 only.
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <asm/mach/arch.h>
+#include <asm/mach-types.h>
+#include <asm/mach/map.h>
+#include <asm/hardware/cache-l2x0.h>
+
+static const char *const npcm7xx_dt_match[] = {
+	"nuvoton,npcm750",
+	NULL
+};
+
+DT_MACHINE_START(NPCM7XX_DT, "NPCM7XX Chip family")
+	.atag_offset	= 0x100,
+	.dt_compat	= npcm7xx_dt_match,
+	.l2c_aux_val	= 0x0,
+	.l2c_aux_mask	= ~0x0,
+MACHINE_END
