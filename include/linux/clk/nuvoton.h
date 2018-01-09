d0344718b809f736158eade663b8ad06d3c3e590
Tomer Maimon <tmaimon77@gmail.com>
2018-01-10 00:46:43 +0200

    clk: npcm: add NPCM7xx clock driver
    Add Nuvoton BMC NPCM7xx clock bus driver.
    
    Signed-off-by: Tomer Maimon <tmaimon77@gmail.com>

---
+++ include/linux/clk/nuvoton.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2014-2018 Nuvoton Technology corporation.
+ *
+ * Released under the GPLv2 only.
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#ifndef __LINUX_CLK_NUVOTON_H_
+#define __LINUX_CLK_NUVOTON_H_
+
+void nuvoton_npcm750_clock_init(void);
+
+#endif
